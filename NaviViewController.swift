2e3ebb61e593b8ac5992004140a56a7077265148
Zhcyn <zhuoh_summer@163.com>
2019-08-27 14:53:01 +0800

    Add files via upload

---
+++ NaviViewController.swift
@@ -0,0 +1,29 @@
+//
+//  NaviViewController.swift
+//  GoStronger
+//
+//  Created by summer on 2019/8/27.
+//
+
+import UIKit
+
+class NaviViewController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
